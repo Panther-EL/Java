e7f5f9f380998486eeb92d83fd723044e4c0720c
Elorm <agbekoelorm32@gmail.com>
2025-07-26 22:57:37 +0000

    Add Js.js file

---
+++ JS/day2.js
@@ -0,0 +1,20 @@
+
+let commskills1 = 3;
+let programming1 = 4;
+let math1 = 3;
+function cwa (commskills,programming,math ){
+    let cwaVal =(commskills1*commskills+programming1*programming+math*math1)/10
+    return cwaVal;
+}
+console.log(cwa(70,80,90))
+
+let commskills = 3;
+let programming = 4;
+let math = 3;
+function cwa (commskills, programming,math){
+    
+}
+let fruit = 'apple';
+let bag = {
+  [fruit + 'Computers']: 5 // bag.appleComputers = 5
+};
